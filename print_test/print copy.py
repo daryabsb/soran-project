78573dd8ff207f86b531f416329b54c6baa33742
Darya Ibrahim <daryabsb@gmail.com>
2022-12-15 17:10:32 +0300

    playground with print

--- print_test/print copy.py
+++ print_test/print copy.py
@@ -6,15 +6,6 @@ import tempfile
 import time
 import win32api  # not included in requirements.txt
 import win32print
-import win32ui
-
-from ctypes import *
-
-def get_active_printer(index=2) -> str:
-    all_printers = [printer[2] for printer in win32print.EnumPrinters(2)]
-    print(all_printers[index])
-    return all_printers[index]
-
 
 
 def generate_qr_code(qr_input: str):
@@ -34,7 +25,6 @@ def print_placard():
     inv['my_qr'] = (open(generate_qr_code("12345A"), 'rb'), 'image/png')
 
     template_path = os.path.dirname(os.path.abspath(__file__))
-    # template_file = r'basic.odt'
     template_file = r'basic.odt'
 
     filepath = os.path.join(template_path, template_file)
@@ -44,21 +34,11 @@ def print_placard():
 
     with tempfile.NamedTemporaryFile(delete=False, suffix='.odt') as tmpfile:
         output = basic.generate(o=inv).render().getvalue()
-        # print("output", output)
-        tmpfile.write(output)  # /tmp in GNU/Linux,  %temp% in Windows 
-        active_printer = get_active_printer(0)
-        printer = win32print.OpenPrinter(active_printer)      
+        print("output", output)
+        tmpfile.write(output)  # /tmp in GNU/Linux,  %temp% in Windows        
         time.sleep(2)
-        # print(f'Printing: {tmpfile.name}')
-        # win32api.ShellExecute(0, 'print', tmpfile.name, f'/d:"{win32print.GetDefaultPrinter()}"', '.', 0)
-        win32api.ShellExecute(0, 'printto', tmpfile.name, f'/d:"{printer}"', '.', 0)
-        win32print.ClosePrinter(printer)
-
+        print(f'Printing: {tmpfile.name}')
+        win32api.ShellExecute(0, 'print', tmpfile.name, f'/d:"{win32print.GetDefaultPrinter()}"', '.', 0)
 
 if __name__ == "__main__":
     print_placard()
-    # win32ui.CreatePopupMenu()
-    # mgm = open(r"mgm.dll")
-    # print(type(mgm))
-    # ctypes.WinDLL ("c:\\PComm\\ehlapi32.dll")
-    # win32ui.CreateDialog(31,'mgm.dll')
